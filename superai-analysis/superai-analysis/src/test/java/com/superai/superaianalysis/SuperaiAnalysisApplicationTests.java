6cd16196d341490c83f3b3b9f1fd45ac80125aa6
xbb <17637283499@163.com>
2025-06-18 17:54:55 +0800

    初始化

---
+++ superai-analysis/superai-analysis/src/test/java/com/superai/superaianalysis/SuperaiAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.superai.superaianalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperaiAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
