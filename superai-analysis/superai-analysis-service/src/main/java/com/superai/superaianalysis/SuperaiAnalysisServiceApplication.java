5ab8ed1bfc5fb2f70025ef71c1b42f87cdc95674
xbb <17637283499@163.com>
2025-06-19 09:16:51 +0800

    初始化

--- superai-analysis/superai-analysis-service/src/main/java/com/superai/superaianalysis/SuperaiAnalysisServiceApplication.java
+++ superai-analysis/superai-analysis-service/src/main/java/com/superai/superaianalysis/SuperaiAnalysisServiceApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SuperaiAnalysisApplication {
+public class SuperaiAnalysisServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SuperaiAnalysisApplication.class, args);
+        SpringApplication.run(SuperaiAnalysisServiceApplication.class, args);
     }
 
 }
